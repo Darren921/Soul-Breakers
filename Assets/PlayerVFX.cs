99a09a22a6ffdf813eeecd59c717b571c776e2a2
Darren921 <darren.chung@ontariotechu.net>
2026-03-30 17:06:32 -0400

    wdaawd

--- Assets/PlayerVFX.cs
+++ Assets/PlayerVFX.cs
@@ -5,8 +5,10 @@ using UnityEngine.VFX;
 public class PlayerVFX : MonoBehaviour
 {
    [SerializeField] VisualEffect JumpEffect;
-
+   [SerializeField] GameObject FireEffect;
+   
    private VFXEventAttribute eventAttribute;
+
    private GameManager gameManager;
 
    private void Start()
@@ -19,5 +21,14 @@ public class PlayerVFX : MonoBehaviour
    {
 //      Debug.Log("PlayJumpEffect");
       JumpEffect.SendEvent(player == gameManager.players[0] ? "PlayJumpEffectP1" : "PlayJumpEffectP2", eventAttribute);
+   }
+
+   public void PlayFireEffect()
+   {
+      FireEffect?.SetActive(true);
+   }
+   public void StopFireEffect()
+   {
+      FireEffect?.SetActive(false);
    }
 }
